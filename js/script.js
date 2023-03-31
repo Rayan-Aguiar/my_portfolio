3ddc668f1698abd801a737a18b1cebea6458e694
Rayan-Aguiar <rayansiqueira@gmail.com>
2023-03-31 17:30:42 -0300

    add section home

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 const title = document.querySelector('.digitando');
+
+function activeWorld(element){
+    const arrText = element.innerHTML.split('');
+    element.innerHTML = '';
+    arrText.forEach((letra, i) =>{
+        setTimeout(()=>{
+            element.innerHTML += letra;
+        }, 75 * i);
+    })
+}
+
+activeWorld(title);
