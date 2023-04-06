5dfc111389afd1dbc658f6ee2225380ffbc0ed35
Rayan-Aguiar <rayansiqueira@gmail.com>
2023-04-06 12:15:37 -0300

    maked navMenu from mobile

--- js/script.js
+++ js/script.js
@@ -15,7 +15,10 @@ activeWorld(title);
 
 
 const activeMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria')
+
 
 activeMenu.addEventListener('click',()=>{
     activeMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
 })
