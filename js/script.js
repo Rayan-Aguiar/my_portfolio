f687f5d0ccfc3d0d8ffd84832e583c4866d71bd5
Rayan-Aguiar <rayansiqueira@gmail.com>
2023-04-12 21:15:25 -0300

    added JS in section aboutMe

--- js/script.js
+++ js/script.js
@@ -1,24 +1,80 @@
 
-const title = document.querySelector('.digitando');
 
-function activeWorld(element){
-    const arrText = element.innerHTML.split('');
-    element.innerHTML = '';
-    arrText.forEach((letra, i) =>{
-        setTimeout(()=>{
-            element.innerHTML += letra;
-        }, 75 * i);
-    })
+function writeTitle(){
+    function activeWorld(element){
+        const arrText = element.innerHTML.split('');
+        element.innerHTML = '';
+        arrText.forEach((letra, i) =>{
+            setTimeout(()=>{
+                element.innerHTML += letra;
+            }, 75 * i);
+        })
+    }
+    const title = document.querySelector('.digitando');
+    activeWorld(title);
+}
+writeTitle();
+
+
+
+
+function menuMobile(){
+    const activeMenu = document.querySelector('.fa-bars');
+    const navMenu = document.querySelector('header .navegacao-primaria');
+    
+    
+    activeMenu.addEventListener('click',()=>{
+        activeMenu.classList.toggle('fa-x');
+        navMenu.classList.toggle('ativado');
+    });
+
+}
+menuMobile();
+
+function aboutMe(){
+    const divExperience = document.querySelectorAll('.experience_content div'); 
+    const liExperience = document.querySelectorAll('.experience_content ul li'); 
+    const divEducation = document.querySelectorAll('.education_content div'); 
+    const liEducation = document.querySelectorAll('.education_content ul li'); 
+
+    divEducation[0].classList.add('active');
+    liEducation[0].classList.add('active');
+    divExperience[0].classList.add('active');
+    liExperience[0].classList.add('active');
+    
+    function slideShow(index){
+        divExperience.forEach((div)=>{
+            div.classList.remove('active')
+        });
+        liExperience.forEach((buttom)=>{
+            buttom.classList.remove('active');
+        })
+        divExperience[index].classList.add('active');
+        liExperience[index].classList.add('active');
+    }
+    
+    function slideShow2(index){
+        divEducation.forEach((div)=>{
+            div.classList.remove('active')
+        });
+        liEducation.forEach((buttom)=>{
+            buttom.classList.remove('active');
+        })
+        divEducation[index].classList.add('active');
+        liEducation[index].classList.add('active');
+    }
+    
+    liExperience.forEach((event, index) => {
+        event.addEventListener('click', () =>{
+            slideShow(index);
+        })
+    });
+    
+    liEducation.forEach((event, index) => {
+        event.addEventListener('click', () =>{
+            slideShow2(index);
+        })
+    });
 }
 
-activeWorld(title);
-
-
-const activeMenu = document.querySelector('.fa-bars');
-const navMenu = document.querySelector('header .navegacao-primaria')
-
-
-activeMenu.addEventListener('click',()=>{
-    activeMenu.classList.toggle('fa-x')
-    navMenu.classList.toggle('ativado')
-})
+aboutMe();
